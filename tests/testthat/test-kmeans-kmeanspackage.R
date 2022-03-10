e5dcd84c4e1d5e58ef6259fa33926f5c4e2acd75
akhilbandhu <akhilbandhu@gmail.com>
2022-03-09 20:15:17 -0700

    testing added

--- tests/testthat/test-kmeans-kmeanspackage.R
+++ tests/testthat/test-kmeans-kmeanspackage.R
@@ -1,7 +1,23 @@
-input_data <- iris[, c(1, 2)]
-input_clusters <- 3
+input_data1 <- iris[,2]
+input_clusters1 <- 1
 
-kmeans_test <- kmeans(input_data, input_clusters)
-function_test <- kmeans_function(input_data, input_clusters)
+input_data2 <- iris[,3]
+input_clusters2 <- 1
+
+input_data3 <- iris[,4]
+input_clusters3 <- 1
+
+kmeans_test <- kmeans(input_data1, input_clusters1)
+function_test <- kmeans_function(input_data1, input_clusters1)
 
 expect_equal(kmeans_test, function_test)
+
+expect_equal(
+  kmeans(input_data2, input_clusters2),
+  kmeans_function(input_data2, input_clusters2)
+)
+
+expect_equal(
+  kmeans(input_data3, input_clusters3),
+  kmeans_function(input_data3, input_clusters3)
+)
